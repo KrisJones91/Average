b48202145e9e20124b8e3c8bc9899bab30438690
Kris Jones <kristop.jones@gmail.com>
2021-06-09 15:46:29 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace random
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
+            
+            while(input != "-1")
+            {
+                Console.WriteLine("Last number was {0}", currentNumber);
+                Console.WriteLine("Please enter the next score");
+                Console.WriteLine("Current amount of entries {0}", count);
+                Console.WriteLine("Please enter -1 once you are ready to calculate the average.");
+
+                input = Console.ReadLine();
+                
+            }
+            
+            
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
